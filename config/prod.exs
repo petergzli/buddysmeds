7b80d26a44bccde3329d04b183613fee341f14e6
Peter Li <petergzli@gmail.com>
2017-11-29 15:09:41 -0800

    Setting up distillery

--- config/prod.exs
+++ config/prod.exs
@@ -18,6 +18,7 @@ config :buddysmeds, Buddysmeds.Endpoint,
 
 # Do not print debug messages in production
 config :logger, level: :info
+config :phoenix, :serve_endpoints, true
 
 # ## SSL Support
 #
