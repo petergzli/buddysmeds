b27294df1f9afece25cd5690f8df86a7fe4b8963
Peter Li <petergzli@gmail.com>
2017-12-04 14:43:19 -0800

    SSL changes

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :buddysmeds, Buddysmeds.Endpoint,
   http: [port: 8888],
-  url: [host: "example.com", port: 80],
+  url: [host: "buddysmeds.com", port: 443, scheme: "https"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
