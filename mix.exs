2f515746560e3f41f326dbd698ce01b2ff32cb15
Peter Li <petergzli@gmail.com>
2018-03-03 22:33:25 -0800

    updates to site

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Buddysmeds.Mixfile do
   def project do
     [
       app: :buddysmeds,
-      version: "0.0.23",
+      version: "0.0.24",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
